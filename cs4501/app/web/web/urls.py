05f0b87bdafc5f3ef70a78b1fdcc3eecdbb746cc
Machoper <qianyixuaneric@gmail.com>
2017-10-04 13:40:48 -0400

    created homepage and detailpage

--- cs4501/app/web/web/urls.py
+++ cs4501/app/web/web/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
+    url(r'computerdetail/(?P<computer_id>[0-9]+)', views.car_detail, name='computer_detail'),
 ]
--- cs4501/app/web/web/views.py
+++ cs4501/app/web/web/views.py
@@ -1,6 +1,28 @@
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.template import loader
 from .models import Computer, Review, User
+import urllib.request, json
 
 def index(request):
-	return render(request, ‘home.html')
+	template = loader.get_template('home.html')
+	context = {}
+	if request.method != 'GET':
+		return HttpResponse(template.render(context, request))
+	try:
+		req = urllib.request.Request('http://exp-api:8000/api/v1/home')
+	except e:
+		return HttpResponse(template.render(context, request))
+	resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+	resp = json.loads(resp_json)
+	context = resp["resp"]
+	return HttpResponse(template.render(context, request))
+
+
+def detail(request, computer_id):
+	req = urllib.request.Request('http://exp-api:8000/exp/v2/computer/' + computer_id + '/')
+	resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+	resp = json.loads(resp_json)
+	#computer = get_object_or_404(Computer, pk=computer_id)
+	return render(request, 'detail.html', resp)
+
