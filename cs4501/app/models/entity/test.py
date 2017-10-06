0afd85f1e48d5f144515b2c41f0fcd72a5f82264
L33t H4x0r <jz7zb@virginia.edu>
2017-10-05 22:17:08 -0400

    updated unit tests

--- cs4501/app/models/entity/test.py
+++ cs4501/app/models/entity/test.py
@@ -10,41 +10,52 @@ class UserTestCase(TestCase):
     def test_createSuccess(self):
         response = self.client.post('/api/v1/user/create/', {'username': 'fred', 'password': 'pass', 'email': 'jz7zb@virginia.edu'})
         self.assertEquals(response.status_code, 200)
+        self.assertEquals(User.objects.count(), 1)
 
     def test_createFail(self):
         response = self.client.post('/api/v1/user/create/', {'username': 'fred', 'password': 'pass'})
         self.assertEquals(response.status_code, 404)
+        self.assertEquals(User.objects.count(), 0)
         
     def test_deleteSuccess(self):
         self.client.post('/api/v1/user/create/', {'username': 'fred', 'password': 'pass', 'email': 'jz7zb@virginia.edu'})
-        response = self.client.get('/api/v1/user/delete/1')
-        self.assertEquals(response.status_code, 301)
+        response = self.client.delete('/api/v1/user/delete/1/')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(User.objects.count(), 0)
     
     def test_deleteFail(self):
-        response = self.client.get('/api/v1/user/delete/100')
-        self.assertEquals(response.status_code, 301)
+        self.client.post('/api/v1/user/create/', {'username': 'fred', 'password': 'pass', 'email': 'jz7zb@virginia.edu'})
+        response = self.client.delete('/api/v1/user/delete/100/')
+        self.assertEquals(response.status_code, 404)
+        self.assertEquals(User.objects.count(), 1)
         
     def test_getSuccess(self):
         self.client.post('/api/v1/user/create/', {'username': 'fred', 'password': 'pass', 'email': 'jz7zb@virginia.edu'})
-        response = self.client.get('/api/v1/user/1')
-        self.assertEquals(response.status_code, 301)
+        response = self.client.get('/api/v1/user/1/')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(User.objects.count(), 1)
         
     def test_getFail(self):
-        response = self.client.get('/api/v1/user/100')
-        self.assertEquals(response.status_code, 301)
+        self.client.post('/api/v1/user/create/', {'username': 'fred', 'password': 'pass', 'email': 'jz7zb@virginia.edu'})
+        response = self.client.get('/api/v1/user/100/')
+        self.assertEquals(response.status_code, 404)
+        self.assertEquals(User.objects.count(), 1)
         
     def test_updateSuccess(self):
         self.client.post('/api/v1/user/create/', {'username': 'fred', 'password': 'pass', 'email': 'jz7zb@virginia.edu'})
         response = self.client.post('/api/v1/user/1/', {'username': 'bob', 'password': 'pass123', 'email': 'bob@virginia.edu'})
         self.assertEquals(response.status_code, 200) 
+        self.assertEquals(User.objects.count(), 1)
     
     def test_updateFail(self):
+        self.client.post('/api/v1/user/create/', {'username': 'fred', 'password': 'pass', 'email': 'jz7zb@virginia.edu'})
         response = self.client.post('/api/v1/user/100/', {'username': 'bob', 'password': 'pass123'})
-        self.assertEquals(response.status_code, 200) 
+        self.assertEquals(response.status_code, 404) 
+        self.assertEquals(User.objects.count(), 1)
         
     def tearDown(self):
         pass
-    
+
 class ComputerTestCase(TestCase):
     
     def setUp(self):
@@ -53,37 +64,48 @@ class ComputerTestCase(TestCase):
     def test_createSuccess(self):
         response = self.client.post('/api/v1/computer/create/', {'make': 'HP', 'model': 'Spectre', 'condition': 'new', 'description': 'really cool'})
         self.assertEquals(response.status_code, 200)
+        self.assertEquals(Computer.objects.count(), 1)
 
     def test_createFail(self):
         response = self.client.post('/api/v1/computer/create/', {'make': 'HP', 'model': 'Spectre', 'condition': 'new'})
         self.assertEquals(response.status_code, 404)
+        self.assertEquals(Computer.objects.count(), 0)
         
     def test_deleteSuccess(self):
         self.client.post('/api/v1/computer/create/', {'make': 'HP', 'model': 'Spectre', 'condition': 'new', 'description': 'really cool'})
-        response = self.client.get('/api/v1/computer/delete/1')
-        self.assertEquals(response.status_code, 301)
+        response = self.client.delete('/api/v1/computer/delete/1/')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(Computer.objects.count(), 0)
     
     def test_deleteFail(self):
-        response = self.client.get('/api/v1/computer/delete/100')
-        self.assertEquals(response.status_code, 301)
+        self.client.post('/api/v1/computer/create/', {'make': 'HP', 'model': 'Spectre', 'condition': 'new', 'description': 'really cool'})
+        response = self.client.delete('/api/v1/computer/delete/100/')
+        self.assertEquals(response.status_code, 404)
+        self.assertEquals(Computer.objects.count(), 1)
         
     def test_getSuccess(self):
         self.client.post('/api/v1/computer/create/', {'make': 'HP', 'model': 'Spectre', 'condition': 'new', 'description': 'really cool'})
-        response = self.client.get('/api/v1/computer/1')
-        self.assertEquals(response.status_code, 301)
+        response = self.client.get('/api/v1/computer/1/')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(Computer.objects.count(), 1)
         
     def test_getFail(self):
-        response = self.client.get('/api/v1/computer/100')
-        self.assertEquals(response.status_code, 301)
+        self.client.post('/api/v1/computer/create/', {'make': 'HP', 'model': 'Spectre', 'condition': 'new', 'description': 'really cool'})
+        response = self.client.get('/api/v1/computer/100/')
+        self.assertEquals(response.status_code, 404)
+        self.assertEquals(Computer.objects.count(), 1)
         
     def test_updateSuccess(self):
         self.client.post('/api/v1/computer/create/', {'make': 'HP', 'model': 'Spectre', 'condition': 'new', 'description': 'really cool'})
         response = self.client.post('/api/v1/computer/1/', {'make': 'Apple', 'model': 'Macbook', 'condition': 'used', 'description': 'awesome'})
         self.assertEquals(response.status_code, 200) 
+        self.assertEquals(Computer.objects.count(), 1)
     
     def test_updateFail(self):
+        self.client.post('/api/v1/computer/create/', {'make': 'HP', 'model': 'Spectre', 'condition': 'new', 'description': 'really cool'})
         response = self.client.post('/api/v1/computer/100/', {'make': 'Apple', 'model': 'Macbook'})
-        self.assertEquals(response.status_code, 200) 
+        self.assertEquals(response.status_code, 404) 
+        self.assertEquals(Computer.objects.count(), 1)
         
     def tearDown(self):
         pass
@@ -97,38 +119,50 @@ class ReviewTestCase(TestCase):
     def test_createSuccess(self):
         response = self.client.post('/api/v1/review/create/', {'rating': 10, 'description': 'great'})
         self.assertEquals(response.status_code, 200)
+        self.assertEquals(Review.objects.count(), 1)
 
     def test_createFail(self):
         response = self.client.post('/api/v1/review/create/', {'rating': 10})
         self.assertEquals(response.status_code, 404)
+        self.assertEquals(Review.objects.count(), 0)
         
     def test_deleteSuccess(self):
         self.client.post('/api/v1/review/create/', {'rating': 10, 'description': 'great'})
-        response = self.client.get('/api/v1/review/delete/1')
-        self.assertEquals(response.status_code, 301)
+        response = self.client.delete('/api/v1/review/delete/1/')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(Review.objects.count(), 0)
     
     def test_deleteFail(self):
-        response = self.client.get('/api/v1/review/delete/100')
-        self.assertEquals(response.status_code, 301)
+        self.client.post('/api/v1/review/create/', {'rating': 10, 'description': 'great'})
+        response = self.client.delete('/api/v1/review/delete/100/')
+        self.assertEquals(response.status_code, 404)
+        self.assertEquals(Review.objects.count(), 1)
         
     def test_getSuccess(self):
         self.client.post('/api/v1/review/create/', {'rating': 10, 'description': 'great'})
-        response = self.client.get('/api/v1/review/1')
-        self.assertEquals(response.status_code, 301)
+        response = self.client.get('/api/v1/review/1/')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(Review.objects.count(), 1)
         
     def test_getFail(self):
-        response = self.client.get('/api/v1/review/100')
-        self.assertEquals(response.status_code, 301)
+        self.client.post('/api/v1/review/create/', {'rating': 10, 'description': 'great'})
+        response = self.client.get('/api/v1/review/100/')
+        self.assertEquals(response.status_code, 404)
+        self.assertEquals(Review.objects.count(), 1)
         
     def test_updateSuccess(self):
         self.client.post('/api/v1/review/create/', {'rating': 10, 'description': 'great'})
         response = self.client.post('/api/v1/review/1/', {'rating': 1, 'description': 'bad'})
         self.assertEquals(response.status_code, 200) 
+        self.assertEquals(Review.objects.count(), 1)
     
     def test_updateFail(self):
+        self.client.post('/api/v1/review/create/', {'rating': 10, 'description': 'great'})
         response = self.client.post('/api/v1/review/100/', {'rating': 5})
-        self.assertEquals(response.status_code, 200) 
+        self.assertEquals(response.status_code, 404) 
+        self.assertEquals(Review.objects.count(), 1)
         
     def tearDown(self):
         pass
+
     
