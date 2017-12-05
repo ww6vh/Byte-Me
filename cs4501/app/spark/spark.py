4397d75f94b4a72224b3834e89cb87603c7d0eef
WendyWu <ww6vh@virginia.edu>
2017-12-05 00:27:37 -0500

    Spark code for data manipulation. There are still duplicates

--- cs4501/app/spark/spark.py
+++ cs4501/app/spark/spark.py
@@ -1,3 +1,5 @@
+
+'''
 from pyspark import SparkContext
 
 sc = SparkContext("spark://spark-master:7077", "PopularItems")
@@ -5,13 +7,44 @@ sc = SparkContext("spark://spark-master:7077", "PopularItems")
 data = sc.textFile("/tmp/data/accessLog.txt", 2)     # each worker loads a piece of the data file
 
 pairs = data.map(lambda line: line.split("\t"))   # tell each worker to split each line of it's partition
-pages = pairs.map(lambda pair: (pair[0], pair[1])).distinct().join(pairs.map(lambda pair: (pair[0], pair[1])).distinct()).distinct()
-output = pages.map(lambda x: (x[1], x[0])).groupByKey()
-output = output.map(lambda x: (x[0], len(x[1]))).filter(lambda x: list(x[0])[0] != list(x[0])[1]).filter(lambda x: x[1] > 2).collect()
+pages = pairs.map(lambda pair: (pair[1], 1))      # re-layout the data to ignore the user id
+count = pages.reduceByKey(lambda x,y: int(x)+int(y))        # shuffle the data so that each key is only on one worker
+                                                  # and then reduce all the values by adding them together
 
-
-for x in output:
-    print(x[0], (x[1]))
+output = count.collect()                          # bring the data back to the master node so we can print it out
+for page_id, count in output:
+    print ("page_id %s count %d" % (page_id, count))
 print ("Popular items done")
 
 sc.stop()
+'''
+
+from pyspark import SparkContext
+from itertools import combinations
+
+sc = SparkContext("spark://spark-master:7077", "CoViews")
+
+data = sc.textFile("/tmp/data/accessLog.txt", 2)     # each worker loads a piece of the data file
+pairs = data.map(lambda line: line.split("\t"))  # tell each worker to split each line of it's partition
+#groups = pairs.map(lambda pair: (str(pair[0]),str(pair[1]) ))
+grouped = pairs.groupByKey().map(lambda x : (x[0], list(x[1])))
+
+transform1 = grouped.flatMap(lambda x: [(x[0], k) for k in list(combinations(x[1], 2))]) 
+transform2 = transform1.map(lambda x: (x[1], x[0]))
+
+
+transform3 = transform2.groupByKey().map(lambda x: (x[0], set(x[1])))
+transform3 = transform3.map(lambda x: (x[0], len(x[1]))) 
+filtered = transform3.filter(lambda x: x[1] > 0).collect()
+
+#output = transform1.collect()
+
+'''
+for user_id, count in output:
+    print ("Products %s count %s" % (user_id, count))
+print ("Popular items done")
+
+'''
+for user_id, count in filtered:
+    print ("Products %s count %d" % (user_id, count))
+print ("Popular items done")
